22ee275c9d55c26b23060e0b63e8a944ff1a4133
Psopis <konevvik05@mail.ru>
2022-01-21 18:44:10 +0700

    init1.2.0

--- src/main/kotlin/com/example/plugins/Sockets.kt
+++ src/main/kotlin/com/example/plugins/Sockets.kt
@@ -1,6 +1,7 @@
 package com.example.plugins
 
 import com.example.directoryObjects.DriverConnection
+import com.example.directoryObjects.busStop
 import io.ktor.application.*
 import io.ktor.http.cio.websocket.*
 import io.ktor.routing.*
@@ -8,15 +9,16 @@ import io.ktor.websocket.*
 import kotlinx.coroutines.channels.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import org.jetbrains.exposed.sql.select
 import java.time.Duration
 import java.util.*
+
 var listsOfChannels = Collections.synchronizedMap<Int, BroadcastChannel<Frame>>(mutableMapOf())
-var connection:DefaultWebSocketSession? = null
-var websocId:String = ""
-var defaultWebsocket = mutableMapOf<String,DefaultWebSocketSession?>()
+var connection: DefaultWebSocketSession? = null
+var websocId: String = ""
+var defaultWebsocket = mutableMapOf<String, DefaultWebSocketSession?>()
 var mutableSet = Collections.synchronizedList<DriverConnection>(mutableListOf())
 fun Application.configureSockets() {
-
 
 
     val stateflow: MutableStateFlow<String> = MutableStateFlow("")
@@ -32,44 +34,42 @@ fun Application.configureSockets() {
 
     routing {
 
-       webSocket("/passenger/{idpassenger}") {
-            val idpassenger = call.parameters["idpassenger"]
-           if (idpassenger == null) close()
+        webSocket("/passenger/{idpassenger}") {
+            val idpassenger = call.parameters["idpassenger"].toString()
+            if (idpassenger == null) close()
 
-           launch{
-               val driver = mutableSet.find { idpassenger == it.id }
+            launch {
+                var driver = mutableSet.find { idpassenger == it.id } ?: DriverConnection()
 
-               driver?.let {
-                   it.channel.consumeEach { frame ->
-                       send(frame.copy())
-                   }
-               }
+
+                println("driver $driver")
+                driver?.let {
+                    it.channel.consumeEach { frame ->
+                        send(frame.copy())
+                        println("frame.copy()  ${frame.copy()}")
+                    }
+                }
 
             }.join()
 
         }
-       webSocket("/driver/{idDriver}") {
+        webSocket("/driver/{idDriver}") {
             val idDriver = call.parameters["idDriver"]
-           if (idDriver == null) close()
-
+            if (idDriver == null) close(CloseReason(CloseReason.Codes.NORMAL, "Driver is null"))
+            else {
 //            val broadcastChannel = Channel<Frame>()
 //            listsOfChannels.put(idDriver, broadcastChannel.broadcast())
-           val driver = mutableSet.find { idDriver == it.id }
-           if (driver != null){
-               driver.connection = this
-               driver.startBroadcast()
-           } else{
-               val newdriver = DriverConnection(idDriver!!, this)
-               newdriver.connection = this
-               newdriver.startBroadcast()
-               mutableSet.add(newdriver)
-           }
-
-
+                var driver = mutableSet.find { idDriver == it.id }
+                if (driver != null) {
+                    driver.connection = this
+                } else {
+                    driver = DriverConnection(idDriver, this)
+                    driver.connection = this
+                    mutableSet.add(driver)
+                }
+                driver.startBroadcast()
+            }
         }
-
-
-
     }
 }
 
