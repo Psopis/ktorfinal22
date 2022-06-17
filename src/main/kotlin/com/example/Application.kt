2c8c435a3dbebd683a2f7007fa51388b7af74eca
psopi <59154239+Psopis@users.noreply.github.com>
2022-06-17 21:36:22 +0700

    1.1.8.mr port

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -12,7 +12,7 @@ import org.jetbrains.exposed.sql.Database
 
 fun main() {
 
-    val port = 9000//Integer.parseInt(System.getenv("PORT"))
+    val port = Integer.parseInt(System.getenv("PORT"))
 //    Database.connect(
 //        "jdbc:postgresql://ec2-54-204-128-96.compute-1.amazonaws.com:5432/d43qcb6atqt206?sslmode=require",
 //        driver = "org.postgresql.Driver",
